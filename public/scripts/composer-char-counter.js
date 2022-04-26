f483d4dabc884d6c7b7805e967866aacaf418076
Robbie Prokop <robbieprokop@gmail.com>
2022-04-26 21:32:45 +0000

    start work on the composer-char-counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,21 @@
+// $(document).ready("blur", (event) => {
+//   console.log(event);
+// });
+
+// $(() => {
+//   console.log(blur);
+// });
+
+$("#tweet-text").on("input", () => {
+  console.log("input is here!");
+});
+
+// $("#idofsomething").on("keydown", () => {
+//   console.log("you pressed a key");
+//   const $input = $("#new-list-item");
+//   const userInput = $input.val();
+//   console.log(userInput);
+
+//   const $li = $("<li>").text(userInput);
+//   $mainlist.prepend($li);
+// });
