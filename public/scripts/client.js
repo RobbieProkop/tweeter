4e14b12e8b1349c16b4fc2c0ee6269277d89b90f
Robbie Prokop <robbieprokop@gmail.com>
2022-04-27 22:37:50 +0000

    refactored client JS. Moved the funcitons to be outside of the document.ready

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,77 +4,77 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const data = [
+  {
+    user: {
+      name: "Buddha",
+      avatars: "https://i.imgur.com/73hZDYK.png",
+      handle: "@Buddha",
+    },
+    content: {
+      text: "Holding on to anger is like grasping a hot coal with the intent of throwing it at someone else; you are the one who gets burned.",
+    },
+    created_at: 1650982193483,
+  },
+  {
+    user: {
+      name: "LaoTzu",
+      avatars: "https://i.imgur.com/nlhLi3I.png",
+      handle: "@Ltzu",
+    },
+    content: {
+      text: "Nature does not hurry, yet everything is accomplished",
+    },
+    created_at: 1650895793483,
+  },
+  {
+    user: {
+      name: "Newton",
+      avatars: "https://i.imgur.com/73hZDYK.png",
+      handle: "@SirIsaac",
+    },
+    content: {
+      text: "If I have seen further it is by standing on the shoulders of giants",
+    },
+    created_at: 1650722993483,
+  },
+];
+
+const createTweetElement = (data) => {
+  $("time.timeago").timeago();
+  const $tweets = $(`<article>
+  <header class="tweet-header">
+  <div class="user">
+  <img src=
+  ${data.user.avatars} alt="" />
+  <h3>${data.user.name}</h3>
+  </div>
+  <h4>${data.user.handle}</h4>
+  </header>
+  <div class="tweet">${data.content.text}</div>
+  
+  <footer>
+  <p>${$.timeago(data.created_at)}</p>
+  <div>
+  <i class="fas fa-solid fa-flag"></i>
+  <i class="fa-solid fa-arrows-rotate"></i>
+  <i class="fas fa-solid fa-heart"></i>
+  </div>
+  </footer>
+  </article>`);
+  return $tweets;
+};
+
+const renderTweets = function (tweets) {
+  for (const tweet of tweets) {
+    $("#tweet-container").append(createTweetElement(tweet));
+  }
+};
+
 $(() => {
-  const data = [
-    {
-      user: {
-        name: "Buddha",
-        avatars: "https://i.imgur.com/73hZDYK.png",
-        handle: "@Buddha",
-      },
-      content: {
-        text: "Holding on to anger is like grasping a hot coal with the intent of throwing it at someone else; you are the one who gets burned.",
-      },
-      created_at: 1650982193483,
-    },
-    {
-      user: {
-        name: "LaoTzu",
-        avatars: "https://i.imgur.com/nlhLi3I.png",
-        handle: "@Ltzu",
-      },
-      content: {
-        text: "Nature does not hurry, yet everything is accomplished",
-      },
-      created_at: 1650895793483,
-    },
-    {
-      user: {
-        name: "Newton",
-        avatars: "https://i.imgur.com/73hZDYK.png",
-        handle: "@SirIsaac",
-      },
-      content: {
-        text: "If I have seen further it is by standing on the shoulders of giants",
-      },
-      created_at: 1650722993483,
-    },
-  ];
-
-  const createTweetElement = (data) => {
-    $("time.timeago").timeago();
-    const $tweets = $(`<article>
-    <header class="tweet-header">
-    <div class="user">
-    <img src=
-    ${data.user.avatars} alt="" />
-    <h3>${data.user.name}</h3>
-    </div>
-    <h4>${data.user.handle}</h4>
-    </header>
-    <div class="tweet">${data.content.text}</div>
-    
-    <footer>
-    <p>${$.timeago(data.created_at)}</p>
-    <div>
-    <i class="fas fa-solid fa-flag"></i>
-    <i class="fa-solid fa-arrows-rotate"></i>
-    <i class="fas fa-solid fa-heart"></i>
-    </div>
-    </footer>
-    </article>`);
-    return $tweets;
-  };
-
-  const renderTweets = function (tweets) {
-    for (const tweet of tweets) {
-      $("#tweet-container").append(createTweetElement(tweet));
-    }
-  };
   renderTweets(data);
 
   $("form").submit(function (event) {
-    alert("handler has been called");
     event.preventDefault();
   });
 });
