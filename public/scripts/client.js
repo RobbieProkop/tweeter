fdccbb572021d3383be270d9fa1574bc85d324af
Robbie Prokop <robbieprokop@gmail.com>
2022-04-28 00:41:10 +0000

    added conditional statements to check if tweet form is empty, or if max character limit has been reached.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,42 +4,7 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// const data = [
-//   {
-//     user: {
-//       name: "Buddha",
-//       avatars: "https://i.imgur.com/73hZDYK.png",
-//       handle: "@Buddha",
-//     },
-//     content: {
-//       text: "Holding on to anger is like grasping a hot coal with the intent of throwing it at someone else; you are the one who gets burned.",
-//     },
-//     created_at: 1650982193483,
-//   },
-//   {
-//     user: {
-//       name: "LaoTzu",
-//       avatars: "https://i.imgur.com/nlhLi3I.png",
-//       handle: "@Ltzu",
-//     },
-//     content: {
-//       text: "Nature does not hurry, yet everything is accomplished",
-//     },
-//     created_at: 1650895793483,
-//   },
-//   {
-//     user: {
-//       name: "Newton",
-//       avatars: "https://i.imgur.com/73hZDYK.png",
-//       handle: "@SirIsaac",
-//     },
-//     content: {
-//       text: "If I have seen further it is by standing on the shoulders of giants",
-//     },
-//     created_at: 1650722993483,
-//   },
-// ];
-
+//creates an element with HTML for when a tweet is made
 const createTweetElement = (data) => {
   $("time.timeago").timeago();
   const $tweets = $(`<article>
@@ -65,11 +30,14 @@ const createTweetElement = (data) => {
   return $tweets;
 };
 
+//grabs all the tweets in the database, sorts them, and adds them to the top of the main container
 const renderTweets = function (tweets) {
   for (const tweet of tweets) {
     $("#tweet-container").prepend(createTweetElement(tweet));
   }
 };
+
+//makes an ajax request to GET the tweets in the database to display them on the screen
 const loadTweets = () => {
   $.ajax({
     type: "GET",
@@ -80,19 +48,30 @@ const loadTweets = () => {
   });
 };
 
+//document.ready
 $(() => {
-  // renderTweets(data);
+  //load all the tweets we currently have
   loadTweets();
 
+  //upon form submission, prevent submit
   $("form").submit(function (event) {
     event.preventDefault();
+    if (Number($(this).find(".counter").text()) < 0) {
+      return alert("Max character limit reached");
+    } else if (Number($(this).find(".counter").text()) >= 140) {
+      return alert("Please enter a tweet!");
+    }
+    //create a string to use
     const str = $("form").serialize();
+    //post that string
     $.ajax({
       type: "POST",
       url: "/tweets/",
       data: str,
     });
+    //empty the tweets that we previously had,
     $("#tweet-container").empty();
+    //add the tweets again, but this time with the new tweet at the top
     loadTweets();
   });
 });
